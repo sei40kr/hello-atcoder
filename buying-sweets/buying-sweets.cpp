90f074ed8162af41c7f5af2bdac53767550e2d0c
Seong Yong-ju <sei40kr@gmail.com>
2018-02-18 11:04:34 +0900

    Add buying-sweets

---
+++ buying-sweets/buying-sweets.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// buying-sweets.cpp
+// author: Seong Yong-ju ( @sei40kr )
+// Copyright (c) 2018 Seong Yong-ju
+
+using namespace std;
+
+int main() {
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  cout << (x - a) % b << endl;
+}
