b7bb2e674f410f3d22c6400efa46ee7410d720cc
Seong Yong-ju <sei40kr@gmail.com>
2018-04-17 22:05:20 +0900

    Add shift-only

---
+++ shift-only/shift-only.hs
@@ -0,0 +1,18 @@
+-- shift-only.hs ---
+
+-- author: Seong Yong-ju <sei40kr@gmail.com>
+
+f :: Int -> Int
+f a =
+  if mod a 2 == 0
+    then 1 + f (quot a 2)
+    else 0
+
+solve :: [Int] -> Int
+solve = minimum . map f
+
+main :: IO ()
+main = do
+  n <- (read :: String -> Int) <$> getLine
+  as <- map (read :: String -> Int) . take n . words <$> getLine
+  print $ solve as
