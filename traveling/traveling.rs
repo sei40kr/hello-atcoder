d882fd591896509b11352be4034904284012335e
Seong Yong-ju <sei40kr@gmail.com>
2018-05-18 11:39:09 +0900

    Add traveling

---
+++ traveling/traveling.rs
@@ -0,0 +1,100 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min, Ordering};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Read, StdoutLock, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use std::str::FromStr;
+
+// traveling.rs --- Traveling
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+#[allow(unused_macros)]
+macro_rules! get {
+  ($t:ty) => {
+    {
+      let mut line: String = String::new();
+      stdin().read_line(&mut line).unwrap();
+      line.trim().parse::<$t>().unwrap()
+    }
+  };
+  ($($t:ty),*) => {
+    {
+      let mut line: String = String::new();
+      stdin().read_line(&mut line).unwrap();
+      let mut iter = line.split_whitespace();
+      (
+        $(iter.next().unwrap().parse::<$t>().unwrap(),)*
+      )
+    }
+  };
+  ($t:ty; $n:expr) => {
+    (0..$n).map(|_|
+                get!($t)
+    ).collect::<Vec<_>>()
+  };
+  ($($t:ty),*; $n:expr) => {
+    (0..$n).map(|_|
+                get!($($t),*)
+    ).collect::<Vec<_>>()
+  };
+  ($t:ty ;;) => {
+    {
+      let mut line: String = String::new();
+      stdin().read_line(&mut line).unwrap();
+      line.split_whitespace()
+        .map(|t| t.parse::<$t>().unwrap())
+        .collect::<Vec<_>>()
+    }
+  };
+  ($t:ty ;; $n:expr) => {
+    (0..$n).map(|_| get!($t ;;)).collect::<Vec<_>>()
+  };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+  ($($a:expr),*) => {
+    eprintln!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+  }
+}
+
+#[allow(dead_code)]
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("Failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("Failed to parse token")
+}
+
+#[allow(dead_code)]
+fn with_bufwriter<F: FnOnce(BufWriter<StdoutLock>) -> ()>(f: F) {
+    let out = stdout();
+    let writer = BufWriter::new(out.lock());
+    f(writer);
+}
+
+fn main() {
+    let n = get!(i32);
+    let mut vs = get!(i32, i32, i32; n);
+    vs.insert(0, (0, 0, 0));
+
+    let ans = vs.windows(2).all(|w| match w {
+        [(t, x, y), (tn, xn, yn)] => {
+            let dt = tn - t;
+            let dist = (xn - x).abs() + (yn - y).abs();
+            dist <= dt && dt % 2 == dist % 2
+        }
+        _ => false,
+    });
+
+    with_bufwriter(|mut out| writeln!(out, "{}", if ans { "Yes" } else { "No" }).unwrap());
+}
