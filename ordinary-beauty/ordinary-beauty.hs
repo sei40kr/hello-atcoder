1c8800676166ec95d2e9d324cbd9ef69bdbdb733
Seong Yong-ju <sei40kr@gmail.com>
2018-07-16 11:14:50 +0900

    Update ordinary-beauty
    Fix the solve expression.

--- ordinary-beauty/ordinary-beauty.hs
+++ ordinary-beauty/ordinary-beauty.hs
@@ -3,8 +3,8 @@
 -- author: Seong Yong-ju <sei40kr@gmail.com>
 
 solve :: Double -> Double -> Double -> Double
-solve n m 0 = (m - 1) / n
-solve n m d = 2 * (n - d) * (m - 1) / (n * n)
+solve n m 0 = 1 / n * (m - 1)
+solve n m d = 2 * (n - d) / (n * n) * (m - 1)
 
 main :: IO ()
 main = do
