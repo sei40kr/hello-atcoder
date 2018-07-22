94b65b11f61b02785a66a77bd07d58995c8812e7
Seong Yong-ju <sei40kr@gmail.com>
2018-07-22 21:33:43 +0900

    Add task-scheduling-problem

---
+++ task-scheduling-problem/task-scheduling-problem.hs
@@ -0,0 +1,15 @@
+import           Data.List
+
+-- task-scheduling-problem.hs ---
+
+-- author: Seong Yong-ju <sei40kr@gmail.com>
+
+solve ::[Int] ->Int
+solve [a1, a2, a3] = (sum . take 2 . sort) ds
+  where
+    ds = [abs $ a1 - a2, abs $ a2 - a3, abs $ a3 - a1]
+
+main :: IO ()
+main = do
+  as <- map (read :: String -> Int) . take 3 . words <$> getLine
+  print $ solve as
