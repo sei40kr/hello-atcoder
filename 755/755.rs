c8c3e3a26da453160e383b6766c4a64110123c23
Seong Yong-ju <sei40kr@gmail.com>
2018-12-08 23:50:16 +0900

    Add 755

---
+++ 755/755.rs
@@ -0,0 +1,104 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min, Ordering};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Read, StdoutLock, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+#[allow(unused_imports)]
+use std::str::FromStr;
+
+// 755.rs --- 755
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+  ($($a:expr),*) => {
+    println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+  }
+}
+
+#[allow(dead_code)]
+fn with_bufwriter<F: FnOnce(BufWriter<StdoutLock>) -> ()>(f: F) {
+    let out = stdout();
+    let writer = BufWriter::new(out.lock());
+    f(writer);
+}
+
+fn dfs(n: i64, cs: String) -> i64 {
+    if n < i64::from_str(&cs).unwrap() {
+        return 0;
+    }
+
+    return (if ['7', '5', '3']
+        .into_iter()
+        .all(|c| cs.chars().collect::<Vec<_>>().contains(c))
+    {
+        1
+    } else {
+        0
+    }) + ['7', '5', '3']
+        .into_iter()
+        .map(|c| dfs(n, format!("{}{}", cs, c.to_string())))
+        .sum::<i64>();
+}
+
+fn main() {
+    input! {
+      n: i64
+    }
+
+    with_bufwriter(|mut out| {
+        writeln!(out, "{}", dfs(n, String::from("0")));
+    });
+}
