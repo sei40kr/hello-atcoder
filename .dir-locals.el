07eac2635939a3b342495dd564526fe80cf6f900
Seong Yong-ju <sei40kr@gmail.com>
2018-12-15 02:36:57 +0900

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,9 +4,12 @@
 
 ;; Author: Seong Yong-ju <sei40kr@gmail.com>
 
-((haskell-mode .
-   (
-     (quickrun-option-cmd-alist .
-       (
-         (:command . "stack")
-         (:exec "%c runghc %o %s %a"))))))
+((haskell-mode . ((quickrun-option-cmd-alist . ((:command . "stack")
+                                                 (:exec "%c runghc %o %s %a")))))
+  (rust-mode . ((eval . (flycheck-disable-checker 'rust))
+                 (quickrun-option-cmd-alist . ((:command . "rustup")
+                                                (:exec . ("%c command --install 1.15.1 rustc %o -o %e %s" "%e %a"))
+                                                (:compile-only . "%c command --install 1.15.1 rustc %o -o %e %s")
+                                                (:remove . ("%e"))
+                                                (:description . "Compile rust and execute via rustup")))
+                 (racer-rust-src-path . "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src"))))
