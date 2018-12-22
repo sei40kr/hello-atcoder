72bbca817e840c9f8f156dea4e10594ee46ec60e
Seong Yong-ju <sei40kr@gmail.com>
2018-12-23 00:38:00 +0900

    Update .dir-locals.el
    Enable rust flycheck checker. Fix quickrun command for rust-mode.

--- .dir-locals.el
+++ .dir-locals.el
@@ -6,10 +6,11 @@
 
 ((haskell-mode . ((quickrun-option-cmd-alist . ((:command . "stack")
                                                  (:exec "%c runghc %o %s %a")))))
-  (rust-mode . ((eval . (flycheck-disable-checker 'rust))
-                 (quickrun-option-cmd-alist . ((:command . "rustup")
-                                                (:exec . ("%c command --install 1.15.1 rustc %o -o %e %s" "%e %a"))
-                                                (:compile-only . "%c command --install 1.15.1 rustc %o -o %e %s")
-                                                (:remove . ("%e"))
-                                                (:description . "Compile rust and execute via rustup")))
-                 (racer-rust-src-path . "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src"))))
+  (rust-mode . ((racer-rust-src-path .
+                  "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src")
+                 (quickrun-option-cmd-alist .
+                   ((:command . "rustup")
+                     (:exec . ("%c run --install 1.15.1 rustc %o -o %e %s" "%e %a"))
+                     (:compile-only . "%c run --install 1.15.1 rustc %o -o %e %s")
+                     (:remove . ("%e"))
+                     (:description . "Compile rust and execute via rustup"))))))
