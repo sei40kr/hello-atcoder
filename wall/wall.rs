71ba7dd29d39947c3c2d13680a3d3335706f68ce
Seong Yong-ju <sei40kr@gmail.com>
2018-12-23 14:29:35 +0900

    Add wall

---
+++ wall/wall.rs
@@ -0,0 +1,87 @@
+use std::cmp::min;
+
+// wall.rs --- Wall
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// tanakah's input macro
+// cf https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+fn main() {
+    input! {
+        h: usize,
+        w: usize,
+        cs: [[i32; 10]; 10],
+        xs: [i32; h * w]
+    }
+
+    let mut wf = cs;
+    for k in 0..10 {
+        for i in 0..10 {
+            for j in 0..10 {
+                wf[i][j] = min(wf[i][j], wf[i][k] + wf[k][j])
+            }
+        }
+    }
+
+    let ans = xs
+        .into_iter()
+        .filter(|&x| 0 <= x)
+        .map(|x| wf[x as usize][1])
+        .sum::<i32>();
+    println!("{}", ans);
+}
