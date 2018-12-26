854e0a894d8e4ed8ed6598afa4ee950f73525b52
Seong Yong-ju <sei40kr@gmail.com>
2018-12-27 00:29:25 +0900

    Use run-tests.sh in emacs-quickrun

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,13 +4,24 @@
 
 ;; Author: Seong Yong-ju <sei40kr@gmail.com>
 
-((haskell-mode . ((quickrun-option-cmd-alist . ((:command . "stack")
-                                                 (:exec "%c runghc %o %s %a")))))
-  (rust-mode . ((racer-rust-src-path .
-                  "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src")
-                 (quickrun-option-cmd-alist .
-                   ((:command . "rustup")
-                     (:exec . ("%c run --install 1.15.1 rustc %o -o %e %s" "%e %a"))
-                     (:compile-only . "%c run --install 1.15.1 rustc %o -o %e %s")
-                     (:remove . ("%e"))
-                     (:description . "Compile rust and execute via rustup"))))))
+((haskell-mode . ((eval . (setq quickrun-option-cmd-alist
+                            (list
+                              '(:command . "stack")
+                              (cons :exec
+                                (list
+                                  "%c ghc -- %o -o %e %s"
+                                  (concat (projectile-project-root) "utils/run-tests.sh %e %a")))
+                              '(:compile-only . "%c ghc -- %o -o %e %s")
+                              '(:remove "%e")
+                              '(:description . "Compile Haskell file and execute via stack ghc"))))))
+  (rust-mode . ((racer-rust-src-path . "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src")
+                 (eval . (setq quickrun-option-cmd-alist
+                           (list
+                             '(:command . "rustup")
+                             (cons :exec
+                               (list
+                                 "%c run --install 1.15.1 rustc %o -o %e %s"
+                                 (concat (projectile-project-root) "utils/run-tests.sh %e %a")))
+                             '(:compile-only . "%c run --install 1.15.1 rustc %o -o %e %s")
+                             '(:remove . ("%e"))
+                             '(:description . "Compile rust and execute via rustup")))))))
