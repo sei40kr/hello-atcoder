b33e5a779887c028d652725a5f353dffbf8d795e
Seong Yong-ju <sei40kr@gmail.com>
2018-12-27 00:03:51 +0900

    Add a helper script to run AtCoder test cases

---
+++ utils/run-tests.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -u
+IFS=$'\n\t'
+
+# run-tests.sh --- Run AtCoder test cases
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+red="$(tput setaf 1)"
+green="$(tput setaf 2)"
+yellow="$(tput setaf 3)"
+bold="$(tput bold)"
+reset="$(tput sgr0)"
+
+cmd="$@"
+buffile="$(mktemp)"
+errfile="$(mktemp)"
+
+shopt -s nullglob
+
+for infile in input-*.txt; do
+	outfile="output-${infile:6}"
+	if [[ ! -f $outfile ]]; then
+		continue
+	fi
+
+  echo -n "${infile} - "
+
+	eval "${cmd[@]}" <"$infile" 1>"$buffile" 2>"$errfile"
+	if [[ "$?" != 0 ]]; then
+		cat <<EOM
+${bold}${red}err${reset}
+
+error output:
+$(cat "$errfile")
+
+EOM
+		break
+	fi
+
+	if diff -q "$buffile" "$outfile" >/dev/null; then
+		echo "${bold}${green}ok${reset}"
+	else
+		cat <<EOM
+${bold}${yellow}ng${reset}
+
+expected:
+$(cat "$outfile")
+
+actual:
+$(cat "$buffile")
+
+EOM
+	fi
+done
+
+rm -f "$buffile" "$errfile"
