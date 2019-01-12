9340559481f0d831fa06ce11a343db4c391c09eb
Seong Yong-ju <sei40kr@gmail.com>
2019-01-12 21:22:22 +0900

    Add edpc-coins

---
+++ edpc-coins/edpc-coins.rs
@@ -0,0 +1,93 @@
+#[allow(unused_imports)]
+use std::cmp::{max, min, Ordering};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+// edpc-coins.rs --- Coins
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// tanakah's input macro
+// cf https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        ps: [f64; n],
+    };
+
+    let mut dp = vec![vec![0.0; n + 1]; n + 1];
+    dp[0][0] = 1.0;
+
+    for (i, p) in ps.into_iter().enumerate() {
+        for j in 0..n {
+            // i 枚目までの表が j 枚であるような確率は
+            // i-1 枚目までの表が j-1 枚であるような確率 * p
+            // i-1 枚目までの表が j 枚であるような確率 * (1-p)
+            // の2つの和
+            dp[i + 1][j + 1] += dp[i][j] * p;
+            dp[i + 1][j] += dp[i][j] * (1.0 - p);
+        }
+    }
+
+    let ans = dp[n][((n + 1) / 2)..].into_iter().sum::<f64>();
+    println!("{}", ans);
+}
