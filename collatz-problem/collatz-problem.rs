23a59e6c63c0152b3a4ae1b936d54e98b64ee986
Seong Yong-ju <sei40kr@gmail.com>
2019-01-20 23:17:57 +0900

    Add collatz-problem, grand-garden

---
+++ collatz-problem/collatz-problem.rs
@@ -0,0 +1,95 @@
+#[allow(unused_imports)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+// collatz-problem.rs --- Collatz Problem
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// tanakah's input macro
+// cf https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+fn f(x: u32) -> u32 {
+    if x % 2 == 0 {
+        x / 2
+    } else {
+        3 * x + 1
+    }
+}
+
+fn main() {
+    input! {
+        s: u32,
+    };
+
+    let mut x = s;
+    let mut ans = 1;
+
+    while x != 4 && x != 2 && x != 1 {
+        x = f(x);
+        ans += 1;
+    }
+
+    println!("{}", ans + 3);
+}
