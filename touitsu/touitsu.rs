13c10531e4192f9c45753c2f7ae4708814b039fb
Seong Yong-ju <sei40kr@gmail.com>
2019-01-28 00:00:46 +0900

    Add touitsu

---
+++ touitsu/touitsu.rs
@@ -0,0 +1,92 @@
+#[allow(unused_imports)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+// touitsu.rs --- Touitsu
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+// tanakah's input macro
+// cf https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+fn main() {
+    input! {
+        n: usize,
+        a: chars,
+        b: chars,
+        c: chars,
+    };
+
+    let ans = (0..n)
+        .map(|i| {
+            let (p, q, r) = (a[i], b[i], c[i]);
+            let ab = if p != q { 1 } else { 0 };
+            let bc = if q != r { 1 } else { 0 };
+            let ca = if r != p { 1 } else { 0 };
+            min(min(ab + bc, bc + ca), ca + ab)
+        })
+        .sum::<usize>();
+
+    println!("{}", ans);
+}
