aab73013c51da92d16fa0ee7a41f699b5b6ca2d1
Seong Yong-ju <sei40kr@gmail.com>
2019-05-20 22:00:21 +0900

    Add walk-and-teleport

---
+++ arc067/D/main.rs
@@ -0,0 +1,92 @@
+#[allow(unused_imports)]
+use std::cmp::Ordering::{Equal, Greater, Less};
+#[allow(unused_imports)]
+use std::cmp::{max, min};
+#[allow(unused_imports)]
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::{stdin, stdout, BufWriter, Write};
+#[allow(unused_imports)]
+use std::iter::FromIterator;
+
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), " = {:?}, "),*), $($a),*);
+    }
+}
+
+fn main() {
+    let n: i64;
+    let a: i64;
+    let b: i64;
+    let xs: Vec<i64>;
+
+    input! {
+        n: i64,
+        a: i64,
+        b: i64,
+        xs: [i64; n as usize],
+    };
+
+    let ans = xs
+        .windows(2)
+        .into_iter()
+        .map(|pair| pair[1] - pair[0])
+        .map(|dist| dist * a)
+        .map(|dist| if b < dist { b } else { dist })
+        .sum::<i64>();
+
+    println!("{}", ans);
+}
