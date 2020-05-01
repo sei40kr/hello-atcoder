a3685fc5d794623322ab61267625fea442d7f526
Seong Yong-ju <sei40kr@gmail.com>
2020-05-01 14:28:43 +0900

    chore: upgrade rust to v1.42.0

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,12 +4,10 @@
 
 ;; Author: Seong Yong-ju <sei40kr@gmail.com>
 
-((rustic-mode
-  . ((rustic-racer-rust-src-path . "~/.rustup/toolchains/1.15.1-x86_64-apple-darwin/lib/rustlib/src/rust/src")
-     (quickrun-option-cmd-alist
-      . ((:command . "rustup")
-         (:exec    . ("%c run --install 1.15.1 rustc %o -O -o %e %s"
-                      "RUST_BACKTRACE=1 atcoder-tools test -e %e -d %d"))
-         (:remove . ("%e"))
-         (:tempfile . nil)
-         (:description . "Compile Rust file with Rustup/rustc and test with atcoder-tools"))))))
+((rustic-mode . ((lsp-rust-sysroot .  "~/.rustup/toolchains/1.42.0-x86_64-apple-darwin")
+                 (quickrun-option-cmd-alist . ((:command . "rustup")
+                                               (:exec    . ("%c run --install 1.42.0 rustc %o -O -o %e %s"
+                                                            "RUST_BACKTRACE=1 atcoder-tools test -e %e -d %d"))
+                                               (:remove . ("%e"))
+                                               (:tempfile . nil)
+                                               (:description . "Compile Rust file with Rustup/rustc and test with atcoder-tools"))))))
