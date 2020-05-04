eea2a5ad6e968a14c8037dbfa23290d4a46b4bbe
Seong Yong-ju <sei40kr@gmail.com>
2020-05-04 11:06:49 +0900

    chore: disable rust-analyzer

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,10 @@
 
 ;; Author: Seong Yong-ju <sei40kr@gmail.com>
 
-((rustic-mode . ((lsp-rust-sysroot .  "~/.rustup/toolchains/1.42.0-x86_64-apple-darwin")
+((rustic-mode . ((rustic-lsp-server . rls)
+                 (lsp-disabled-clients . (rust-analyzer))
+                 (lsp-rust-server . rls)
+                 (lsp-rust-sysroot .  "~/.rustup/toolchains/1.42.0-x86_64-apple-darwin")
                  (quickrun-option-cmd-alist . ((:command . "rustup")
                                                (:exec    . ("%c run --install 1.42.0 rustc %o -O -o %e %s"
                                                             "RUST_BACKTRACE=1 atcoder-tools test -e %e -d %d"))
